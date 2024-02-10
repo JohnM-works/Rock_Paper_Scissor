24201f0796c3edd55878dec1cdc7f698e9285981
jhin47 <johnmalonzo63@gmail.com>
2024-02-11 03:53:15 +0800

    adding more styles and functions in the game

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,6 @@
 const choices = document.querySelectorAll(".weapon");
-const computerChoice = document.querySelector(".computer-choice");
+const computerChoice = document.querySelector("#computer-output-choice");
+const playerChoice = document.querySelector("#player-output-choice");
 const gameResult = document.querySelector(".result");
 const playAgain = document.querySelector(".play-again");
 const playerResult = document.querySelector(".player-score");
@@ -9,17 +10,33 @@ let playerScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-  let choice = ["rock", "paper", "scissor"];
+  let choice = ["rock", "paper", "scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock") {
+    playerChoice.className = "far fa-hand-rock fa-3x";
+  } else if (playerSelection === "paper") {
+    playerChoice.className = "fa-regular fa-hand fa-3x";
+  } else if (playerSelection === "scissors") {
+    playerChoice.className = "fa-regular fa-hand-scissors fa-3x";
+  }
+
+  if (computerSelection === "rock") {
+    computerChoice.className = "far fa-hand-rock fa-3x";
+  } else if (computerSelection === "paper") {
+    computerChoice.className = "fa-regular fa-hand fa-3x";
+  } else if (computerSelection === "scissors") {
+    computerChoice.className = "fa-regular fa-hand-scissors fa-3x";
+  }
+
   if (playerSelection == computerSelection) {
     gameResult.textContent = "You Tied! Choose weapon again";
   } else if (
     (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissor") ||
-    (playerSelection === "scissor" && computerSelection === "rock")
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
   ) {
     computerScore++;
     gameResult.textContent = `You Lose! ${playerSelection} beats ${computerSelection}`;
@@ -34,10 +51,10 @@ function playRound(playerSelection, computerSelection) {
     gameResult.textContent = "Game Over! You Win the game.";
     disabledWeapon();
   }
-
   if (computerScore === 5) {
     gameResult.textContent = "Game Over! Computer Wins the game.";
     disabledWeapon();
+    outputSelection();
   }
 }
 
@@ -45,8 +62,6 @@ function selectWeapon() {
   const playerSelection = this.id;
   const computerSelection = getComputerChoice();
   playRound(playerSelection, computerSelection);
-  console.log("User: " + playerSelection);
-  console.log("Computer: " + computerSelection);
 }
 
 function resetGame() {
@@ -55,6 +70,8 @@ function resetGame() {
   playerResult.textContent = "Player Score: 0";
   computerResult.textContent = "Computer Score: 0";
   gameResult.textContent = "Choose your weapon!";
+  computerChoice.className = "";
+  playerChoice.className = "";
   EnabledWeapon();
 }
 
